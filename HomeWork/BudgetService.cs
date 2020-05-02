168655bd11bfc2bd75f63f9e8e383dc5a33789f3
Cola.Chen <cola.chen@onelab.tw>
2020-05-02 15:50:46 +0800

    init

---
+++ HomeWork/BudgetService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork
+{
+    public class BudgetService
+    {
+        public decimal Query(DateTime start, DateTime end)
+        {
+            return 0;
+        }
+    }
+}

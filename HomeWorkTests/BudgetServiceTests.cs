8568b328cf3b0fe9e8ad8b33a98ba05fc9fd4075
Cola.Chen <cola.chen@onelab.tw>
2020-05-02 16:37:37 +0800

    April_OneMonthV2

--- HomeWorkTests/BudgetServiceTests.cs
+++ HomeWorkTests/BudgetServiceTests.cs
@@ -11,52 +11,79 @@ namespace HomeWork.Tests
     [TestFixture()]
     public class BudgetServiceTests
     {
-        private BudgetService budgetService;
+        private BudgetService _budgetService;
+        private FakeBudgetRepo _fakeRepo;
 
         [SetUp]
         public void Setup()
         {
-            budgetService = new BudgetService(new FakeBudgetRepo());
+            _fakeRepo = new FakeBudgetRepo();
+            _budgetService = new BudgetService(_fakeRepo);
         }
 
         [Test()]
         public void April_OneDay()
         {
+            _fakeRepo.SetBudgets(new List<Budget>()
+            {
+                new Budget()
+                {
+                    YearMonth = "202004",
+                    Amount    =  30000
+                }
+            }); 
             BudgetShouldBe(1000, new DateTime(2020, 04, 01), new DateTime(2020, 04, 01));
         }
 
-        private void BudgetShouldBe(int expected, DateTime start, DateTime end)
-        {
-            Assert.AreEqual(expected, budgetService.Query(start, end));
-        }
+    
 
         [Test()]
         public void April_MultiDay()
         {
-         
+            _fakeRepo.SetBudgets(new List<Budget>()
+            {
+                new Budget()
+                {
+                    YearMonth = "202004",
+                    Amount    =  30000
+                }
+            });
             BudgetShouldBe(5000,new DateTime(2020,04,01),new DateTime(2020,04,05) );
         }
 
         [Test()]
         public void April_OneMonth()
         {
+            _fakeRepo.SetBudgets(new List<Budget>()
+            {
+                new Budget()
+                {
+                    YearMonth = "202004",
+                    Amount    =  60000
+                }
+            });
+            BudgetShouldBe(60000, new DateTime(2020, 04, 01), new DateTime(2020, 04, 30));
+        }
 
-            BudgetShouldBe(30000, new DateTime(2020, 04, 01), new DateTime(2020, 04, 30));
+        private void BudgetShouldBe(int expected, DateTime start, DateTime end)
+        {
+
+            Assert.AreEqual(expected, _budgetService.Query(start, end));
         }
     }
 
     public class FakeBudgetRepo : IBudgetRepo
     {
+        private List<Budget> _budgetList;
+
+        public void SetBudgets(List<Budget> budgets)
+        {
+            _budgetList = budgets;
+        }
+
         public List<Budget> GetAll()
         {
-            return new List<Budget>()
-            {
-                new Budget()
-                {
-                    YearMonth = "202004",
-                    Amount =  30000
-                }
-            };
+            return _budgetList;
         }
     }
 }

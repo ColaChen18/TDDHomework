135ff4fd7b112d73094cc69eb3ed32b2912d2364
Marsen <thisismysoul@gmail.com>
2020-05-04 13:37:30 +0800

    use days in month

--- HomeWork/BudgetService.cs
+++ HomeWork/BudgetService.cs
@@ -26,19 +26,18 @@ namespace HomeWork
 
         private decimal GetBudget(DateTime start, DateTime end, List<Budget> budgets)
         {
-
             var dayAmountDict = budgets.Select(x => new
             {
                 YearMonth = x.YearMonth,
-                DayAmount = x.Amount / DateTime.DaysInMonth(DateTime.ParseExact(x.YearMonth, "yyyyMM", null).Year, DateTime.ParseExact(x.YearMonth, "yyyyMM", null).Month)
-
+                DayAmount = x.Amount / DateTime.DaysInMonth(DateTime.ParseExact(x.YearMonth, "yyyyMM", null).Year,
+                    DateTime.ParseExact(x.YearMonth, "yyyyMM", null).Month)
             }).ToDictionary(x => x.YearMonth, y => y.DayAmount);
 
 
             if (end.Month - start.Month >= 1)
             {
-
-                int startAmount = (DateTime.DaysInMonth(start.Year, start.Month) - start.Day + 1) * dayAmountDict[start.ToString("yyyyMM")];
+                int startAmount = (DateTime.DaysInMonth(start.Year, start.Month) - start.Day + 1) *
+                                  dayAmountDict[start.ToString("yyyyMM")];
                 int endAmount = (end.Day) * dayAmountDict[end.ToString("yyyyMM")];
 
                 return startAmount + endAmount;
@@ -50,8 +49,7 @@ namespace HomeWork
                 .Where(x => x.YearMonth == start.ToString("yyyyMM"))
                 .Sum(a => a.Amount);
 
-            return amount / 30 * totalDay;
-
+            return amount / DateTime.DaysInMonth(start.Year, start.Month) * totalDay;
         }
 
 

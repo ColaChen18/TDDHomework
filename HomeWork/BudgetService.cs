9b0927dedd7bb50e9c3a4c1a20154bd6f5e2489d
Marsen <thisismysoul@gmail.com>
2020-05-04 16:12:24 +0800

    inline parameter

--- HomeWork/BudgetService.cs
+++ HomeWork/BudgetService.cs
@@ -21,9 +21,8 @@ namespace HomeWork
             {
                 var days = DateTime.DaysInMonth(start.Year, start.Month);
                 var amount = GetBudget(start.ToString("yyyyMM")).Amount;
-                var dailyBudgetOfStart = amount / days;
                 var daysInStartMonth = DateTime.DaysInMonth(start.Year, start.Month) - start.Day + 1;
-                int startAmount = daysInStartMonth * dailyBudgetOfStart;
+                int startAmount = daysInStartMonth * (amount / days);
                 
                 int middleAmount = 0;
                 for (int i = 1; i < end.Month - start.Month; i++)
@@ -32,16 +31,14 @@ namespace HomeWork
 
                     var days1 = DateTime.DaysInMonth(middleDate.Year, middleDate.Month);
                     var amount1 = GetBudget(middleDate.ToString("yyyyMM")).Amount;
-                    var dailyBudgetOfMiddle = amount1 / days1;
                     var daysInMiddleMonth = DateTime.DaysInMonth(middleDate.Year, middleDate.Month);
-                    middleAmount += daysInMiddleMonth * dailyBudgetOfMiddle;
+                    middleAmount += daysInMiddleMonth * (amount1 / days1);
                 }
 
                 var days2 = DateTime.DaysInMonth(end.Year, end.Month);
                 var amount2 = GetBudget(end.ToString("yyyyMM")).Amount;
-                var dailyBudgetOfEnd = amount2 / days2;
                 var daysInEndMonth = (end.Day);
-                int endAmount = daysInEndMonth * dailyBudgetOfEnd;
+                int endAmount = daysInEndMonth * (amount2 / days2);
 
                 return startAmount + middleAmount + endAmount;
             }

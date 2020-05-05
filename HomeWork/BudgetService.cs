f8128349fe8d74211a0ee31f628b4b58d9be58dc
Marsen <thisismysoul@gmail.com>
2020-05-05 10:59:04 +0800

    reduce if

--- HomeWork/BudgetService.cs
+++ HomeWork/BudgetService.cs
@@ -54,15 +54,9 @@ namespace HomeWork
         {
             var budgetFirstDay = new DateTime(date.Year, date.Month, 1);
             var budgetLastDay = new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
-            if (period.End <= budgetLastDay)
-            {
-                var periodEnd = period.End;
-                var periodStart = budgetFirstDay <= period.Start ? period.Start : budgetFirstDay;
-
-                return (periodEnd - periodStart).Days + 1;
-            }
-
-            return 0;
+            var periodEnd = period.End <= budgetLastDay ? period.End : budgetLastDay;
+            var periodStart = budgetFirstDay <= period.Start ? period.Start : budgetFirstDay;
+            return (periodEnd - periodStart).Days + 1;
         }
 
         private static int QueryDaysInEnd(DateTime end)

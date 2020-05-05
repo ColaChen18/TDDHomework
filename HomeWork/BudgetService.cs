15a7c92422133d8cc101b3851c60a3e553fee24b
Marsen <thisismysoul@gmail.com>
2020-05-05 15:43:27 +0800

    inline

--- HomeWork/BudgetService.cs
+++ HomeWork/BudgetService.cs
@@ -23,8 +23,7 @@ namespace HomeWork
             {
                 var month = start.AddMonths(i);
                 var budget = GetBudget(month);
-                var queryDaysInPeriod = period.OverlapDays(new Period(budget.BudgetFirstDay(), budget.BudgetLastDay()));
-                result += queryDaysInPeriod * budget.GetDailyAmount();
+                result += period.OverlapDays(new Period(budget.BudgetFirstDay(), budget.BudgetLastDay())) * budget.GetDailyAmount();
             }
 
             return result;
